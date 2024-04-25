737d95e220cd3363e46b633557566f5dbc0a48cf
cheick18 <97468180+cheick18@users.noreply.github.com>
2024-04-25 14:56:39 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -9,7 +9,7 @@ $database = 'gestion';
 $conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+    die("Erreur de connexion à la base de données : ".$conn->connect_error);
 }
 
 /*
